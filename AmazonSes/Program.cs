995eff9f9105f87c8ff03eefba388c8ff73016c5
MazaharAhamed <mazaharahamed77@gmail.com>
2020-11-09 20:28:38 +0530

    Send multiple emails.

--- AmazonSes/Program.cs
+++ AmazonSes/Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using Amazon.SimpleEmail;
 using Amazon.SimpleEmail.Model;
+using System.Diagnostics;
 
 namespace AmazonSes
 {
@@ -13,10 +14,13 @@ namespace AmazonSes
         // This address must be verified with Amazon SES.
         static readonly string senderAddress = "testemailinn@gmail.com";
 
+
         // Replace recipient@example.com with a "To" address. If your account
         // is still in the sandbox, this address must be verified.
 
-        static readonly string receiverAddress = "mazaharahamed77@gmail.com";
+        //static public string receiverAddress { get; set; }
+
+        static public List<string> emList { get; set; }
 
 
         // The configuration set to use for this email. If you do not want to use a
@@ -24,7 +28,6 @@ namespace AmazonSes
         // ConfigurationSetName = configSet argument below. 
         //static readonly string configSet = "ConfigSet";
 
-        // The subject line for the email.
         static readonly string subject = "Amazon SES test (AWS SDK for .NET)";
 
         // The email body for recipients with non-HTML email clients.
@@ -46,59 +49,69 @@ namespace AmazonSes
 
         static void Main(string[] args)
         {
-            var emailList = new List<string>();
-            for (int i = 1; i <= 100; i++)
+            var watch = new Stopwatch();
+
+            watch.Start();
+            emList = new List<string>();
+
+            for (int i = 1; i <= 2; i++)
             {
-                emailList.Add($"test{i}+mytestemail@gmail.com");
+                //emList.Add($"test{i}+mytestemail@gmail.com");
+                emList.Add($"testemailinn{i}@gmail.com");
             }
             // Replace USWest2 with the AWS Region you're using for Amazon SES.
             // Acceptable values are EUWest1, USEast1, and USWest2.
-            using (var client = new AmazonSimpleEmailServiceClient(RegionEndpoint.USWest2))
+            foreach (var item in emList)
             {
-                var sendRequest = new SendEmailRequest
+                using (var client = new AmazonSimpleEmailServiceClient(RegionEndpoint.USWest2))
                 {
-                    Source = senderAddress,
-                    Destination = new Destination
+                    var sendRequest = new SendEmailRequest
                     {
-                        //ToAddresses =
-                        //new List<string> { receiverAddress }
-
-                        ToAddresses = emailList
-                    },
-                    Message = new Message
-                    {
-                        Subject = new Content(subject),
-                        Body = new Body
+                        Source = senderAddress,
+                        Destination = new Destination
                         {
-                            Html = new Content
-                            {
-                                Charset = "UTF-8",
-                                Data = htmlBody
-                            },
-                            Text = new Content
-                            {
-                                Charset = "UTF-8",
-                                Data = textBody
-                            }
-                        }
-                    },
-                    // If you are not using a configuration set, comment
-                    // or remove the following line 
-                    //ConfigurationSetName = configSet
-                };
-                try
-                {
-                    Console.WriteLine("Sending email using Amazon SES...");
-                    var response = client.SendEmail(sendRequest);
-                    Console.WriteLine("The email was sent successfully.");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("The email was not sent.");
-                    Console.WriteLine("Error message: " + ex.Message);
+                            ToAddresses =
+                            new List<string> { item }
 
+                            //ToAddresses = item
+                        },
+                        Message = new Message
+                        {
+                            Subject = new Content(subject),
+                            Body = new Body
+                            {
+                                Html = new Content
+                                {
+                                    Charset = "UTF-8",
+                                    Data = htmlBody
+                                },
+                                Text = new Content
+                                {
+                                    Charset = "UTF-8",
+                                    Data = textBody
+                                }
+                            }
+                        },
+                        // If you are not using a configuration set, comment
+                        // or remove the following line 
+                        //ConfigurationSetName = configSet
+                    };
+                    try
+                    {
+                        Console.WriteLine("Sending email using Amazon SES...");
+                        var response = client.SendEmail(sendRequest);
+                        Console.WriteLine("The email was sent successfully.");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("The email was not sent.");
+                        Console.WriteLine("Error message: " + ex.Message);
+
+                    }
                 }
             }
+            watch.Stop();
+            Console.WriteLine($"It took {watch.ElapsedMilliseconds} seconds to send 100 mails");
 
             Console.Write("Press any key to continue...");
             Console.ReadKey();
